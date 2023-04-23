be8c7a97fe2d3ff9e4d8fd5af64410fcfbedf3c8
Comrade Thursday <Comrade Thursday@DESKTOP-85T9MMA>
2023-04-23 19:34:02 +0500

    Добавил ползунки(пока без функционала) Ползунки изменяются вместе с разрешением. Добавил ПокраскуПотопом. Карта теперь не скейлится.

--- PaxApocalytica/PaxApocalyticaGame.Designer.cs
+++ PaxApocalytica/PaxApocalyticaGame.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             splitterVertical = new SplitContainer();
+            hScrollBar2 = new HScrollBar();
+            vScrollBar1 = new VScrollBar();
+            hScrollBar1 = new HScrollBar();
             map = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)splitterVertical).BeginInit();
             splitterVertical.Panel1.SuspendLayout();
@@ -45,17 +48,41 @@
             // 
             // splitterVertical.Panel1
             // 
+            splitterVertical.Panel1.Controls.Add(hScrollBar2);
+            splitterVertical.Panel1.Controls.Add(vScrollBar1);
+            splitterVertical.Panel1.Controls.Add(hScrollBar1);
             splitterVertical.Panel1.Controls.Add(map);
             splitterVertical.Size = new Size(1264, 681);
             splitterVertical.SplitterDistance = 900;
             splitterVertical.TabIndex = 0;
             splitterVertical.SplitterMoved += splitterVertical_SplitterMoved;
             // 
+            // hScrollBar2
+            // 
+            hScrollBar2.Location = new Point(0, 661);
+            hScrollBar2.Name = "hScrollBar2";
+            hScrollBar2.Size = new Size(900, 17);
+            hScrollBar2.TabIndex = 3;
+            // 
+            // vScrollBar1
+            // 
+            vScrollBar1.Location = new Point(0, 17);
+            vScrollBar1.Name = "vScrollBar1";
+            vScrollBar1.Size = new Size(17, 644);
+            vScrollBar1.TabIndex = 2;
+            // 
+            // hScrollBar1
+            // 
+            hScrollBar1.Location = new Point(0, 0);
+            hScrollBar1.Name = "hScrollBar1";
+            hScrollBar1.Size = new Size(900, 17);
+            hScrollBar1.TabIndex = 1;
+            // 
             // map
             // 
             map.Location = new Point(3, 3);
             map.Name = "map";
-            map.Size = new Size(100, 50);
+            map.Size = new Size(8192, 4096);
             map.SizeMode = PictureBoxSizeMode.Zoom;
             map.TabIndex = 0;
             map.TabStop = false;
@@ -80,5 +107,8 @@
 
         private SplitContainer splitterVertical;
         private PictureBox map;
+        private HScrollBar hScrollBar1;
+        private VScrollBar vScrollBar1;
+        private HScrollBar hScrollBar2;
     }
 }
--- PaxApocalytica/PaxApocalyticaGame.cs
+++ PaxApocalytica/PaxApocalyticaGame.cs
@@ -12,19 +12,22 @@ namespace PaxApocalytica
 {
     public partial class PaxApocalyticaGame : Form
     {
-        static Bitmap bitmap = new Bitmap(@"C:\Users\Comrade Thursday\source\repos\PaxApocalytica\PaxApocalytica\mapExperimental.bmp", true);
+        static Bitmap bitmap = new Bitmap(@"C:\Users\Comrade Thursday\source\repos\PaxApocalytica\PaxApocalytica\iv74j8jqih881_1.png", true);
+
+        bool toggleUp = false;
+        bool toggleDown = false;
         public PaxApocalyticaGame()
         {
             InitializeComponent();
             map.Image = bitmap;
-            map.Height = splitterVertical.Panel1.Height;
-            map.Width = splitterVertical.Panel1.Width;
         }
 
         protected override void OnResize(EventArgs e)
         {
             ResizeSplitter();
-            ResizeMap();
+            ResizeHorizontalScrollbar();
+            ResizeVerticalScrollbar();
+            //ResizeMap();
         }
         private void splitterVertical_SplitterMoved(object sender, SplitterEventArgs e)
         {
@@ -51,14 +54,81 @@ namespace PaxApocalytica
             }
             splitterVertical.SplitterDistance = (this.Width - 360);
         }
-        public void ResizeMap() 
+        public void ResizeMap()
         {
             map.Height = splitterVertical.Panel1.Height;
             map.Width = splitterVertical.Panel1.Width;
         }
+
+        private void ResizeHorizontalScrollbar()
+        {
+            hScrollBar1.Width = splitterVertical.Panel1.Width;
+            hScrollBar2.Width = splitterVertical.Panel1.Width;
+        }
+
+        private void ResizeVerticalScrollbar()
+        {
+            vScrollBar1.Height = splitterVertical.Height - 34;
+            hScrollBar2.Location = new Point(0, splitterVertical.Height-17);
+        }
         private void map_Click(object sender, EventArgs e)
         {
+            MouseEventArgs mea = (MouseEventArgs)e;
+            int x = mea.X;
+            int y = mea.Y;
+            Color oldColor = bitmap.GetPixel(x, y);
+            FloodFill(bitmap, new Point(x, y), oldColor, Color.Red);
+        }
 
+        private void FloodFill(Bitmap bmp, Point pt, Color targetColor, Color replacementColor)
+        {
+            targetColor = bmp.GetPixel(pt.X, pt.Y);
+            if (targetColor.ToArgb().Equals(replacementColor.ToArgb()))
+            {
+                return;
+            }
+
+            Stack<Point> pixels = new Stack<Point>();
+
+            pixels.Push(pt);
+            while (pixels.Count != 0)
+            {
+                Point temp = pixels.Pop();
+                int y1 = temp.Y;
+                while (y1 >= 0 && bmp.GetPixel(temp.X, y1) == targetColor)
+                {
+                    y1--;
+                }
+                y1++;
+                bool spanLeft = false;
+                bool spanRight = false;
+                while (y1 < bmp.Height && bmp.GetPixel(temp.X, y1) == targetColor)
+                {
+                    bmp.SetPixel(temp.X, y1, replacementColor);
+
+                    if (!spanLeft && temp.X > 0 && bmp.GetPixel(temp.X - 1, y1) == targetColor)
+                    {
+                        pixels.Push(new Point(temp.X - 1, y1));
+                        spanLeft = true;
+                    }
+                    else if (spanLeft && temp.X - 1 >= 0 && bmp.GetPixel(temp.X - 1, y1) != targetColor)
+                    {
+                        spanLeft = false;
+                    }
+                    if (!spanRight && temp.X < bmp.Width - 1 && bmp.GetPixel(temp.X + 1, y1) == targetColor)
+                    {
+                        pixels.Push(new Point(temp.X + 1, y1));
+                        spanRight = true;
+                    }
+                    else if (spanRight && temp.X < bmp.Width - 1 && bmp.GetPixel(temp.X + 1, y1) != targetColor)
+                    {
+                        spanRight = false;
+                    }
+                    y1++;
+                }
+
+            }
+            map.Refresh();
         }
     }
 }
